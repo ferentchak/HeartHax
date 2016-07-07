b43e4d253ee149ac7966f236acb42b250c159447
Charles Ferentchak <charles@rallydev.com>
2016-07-06 19:05:34 -0600

    bye bye

--- index.js
+++
@@ -1,5 +0,0 @@
-var app = require('./app');
-PORT = process.env.PORT || 2001;
-app.listen(PORT);
-
-console.log('Server started: http://localhost:2001/');
--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-/*** @jsx React.DOM */
-var React = require('react');
-var App = require('./components/app');
-var _ = require('lodash');
-
-React.render(<App />, document.getElementById('main'));
