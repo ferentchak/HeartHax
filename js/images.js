95a5d412700b1cb765ee4a16883bb73628727057
Charles Ferentchak <charles@rallydev.com>
2016-10-23 02:01:38 -0600

    images son updates

--- js/images.js
+++ js/images.js
@@ -1,10 +1,638 @@
-window.photos = [];
-window.photos.push({
-  title:'',
-  url:'',
-  previewUrl:'',
-  keywords:{
-    "girl-in-hat":true,
-    model:true
-  }
-});
+
+window.cyn = "Cynthia Berg";
+
+window.photos = [
+   {
+      "title":"Columbine Taken At Conumndrum Hot Springs",
+      "url":"/img/Fullsize-3.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-3.jpg",
+      "keywords":{
+        "flower":true,
+        "Conumndrum":true,
+        "nature":true
+      }
+   },
+   {
+      "title":"Thai Firedancer",
+      "url":"/img/Fullsize-4.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-4.jpg",
+      "keywords":{
+        "firedancer":true,
+        "people":true
+      }
+   },
+   {
+      "title":"Cynthia on a rock by Leadville",
+      "url":"/img/Fullsize-5.jpg",
+      "hide":true
+      "previewUrl":"/img/preview/ConumndrumLog-5.jpg",
+      "keywords":{
+        "person":cyn
+      }
+   },
+   {
+      "title":"Conumndrum Hot Springs",
+      "url":"/img/Fullsize-6.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-6.jpg",
+      "keywords":{
+        "nature":true,
+        "Conumndrum":true
+      }
+   },
+   {
+      "title":"Ariel City Park ",
+      "url":"/img/Fullsize-7.jpg",
+      "hide":true
+      "previewUrl":"/img/preview/ConumndrumLog-7.jpg",
+      "keywords":{
+        "person":true
+      }
+   },
+   {
+      "title":"Ariel City Park",
+      "url":"/img/Fullsize-8.jpg",
+      "hide":true
+      "previewUrl":"/img/preview/ConumndrumLog-8.jpg",
+      "keywords":{
+        "person":true
+      }
+   },
+   {
+      "title":"Flaming Hula Hoop",
+      "url":"/img/Fullsize-9.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-9.jpg",
+      "keywords":{
+        "person":true
+      }
+   },
+   {
+      "title":"Moab Arches with Cynthia Berg",
+      "url":"/img/Fullsize-10.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-10.jpg",
+      "keywords":{
+        "person":cyn,
+        "Moab":true,
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-11.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-11.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-12.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-12.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-13.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-13.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-14.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-14.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-15.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-15.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-16.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-16.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-17.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-17.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-18.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-18.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-19.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-19.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-20.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-20.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-21.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-21.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-22.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-22.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-23.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-23.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-24.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-24.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-25.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-25.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-26.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-26.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-27.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-27.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-28.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-28.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-29.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-29.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-30.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-30.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-31.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-31.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-32.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-32.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-33.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-33.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-34.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-34.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-35.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-35.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-36.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-36.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-37.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-37.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-38.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-38.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-39.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-39.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-40.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-40.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-41.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-41.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-42.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-42.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-43.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-43.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-44.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-44.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-45.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-45.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-46.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-46.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-47.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-47.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-48.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-48.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-49.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-49.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-50.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-50.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-51.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-51.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-52.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-52.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-53.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-53.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-54.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-54.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-55.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-55.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-56.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-56.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-57.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-57.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-58.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-58.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-59.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-59.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-60.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-60.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-61.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-61.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-62.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-62.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-63.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-63.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-64.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-64.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-65.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-65.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-66.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-66.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-67.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-67.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-68.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-68.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-69.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-69.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-70.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-70.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-71.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-71.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-72.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-72.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-73.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-73.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-74.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-74.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-75.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-75.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-76.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-76.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-77.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-77.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-78.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-78.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-79.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-79.jpg",
+      "keywords":{
+
+      }
+   },
+   {
+      "title":"",
+      "url":"/img/Fullsize-80.jpg",
+      "previewUrl":"/img/preview/ConumndrumLog-80.jpg",
+      "keywords":{
+
+      }
+   }
+];
